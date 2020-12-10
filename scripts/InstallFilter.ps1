ebfc56e5d7d72c3847f141efea98f646e0aba6d9
st4ckh0und <11381503+st4ckh0und@users.noreply.github.com>
2020-12-10 14:42:17 +0100

    added installer scripts

---
+++ scripts/InstallFilter.ps1
@@ -0,0 +1,40 @@
+﻿function Test-RegistryValue {
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        $Path,
+        
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        $Key
+    )
+
+    try {
+        Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Key -ErrorAction Stop | Out-Null
+        return $true
+    } catch {
+        return $false
+    }
+}
+
+$path = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
+$name = "Notification Packages"
+$filt = "ipfilter"
+
+New-Item -Path "C:\" -Name "improsec" -ItemType Directory -Force | Out-Null
+Set-Content -Path "C:\improsec\enabled.txt" -Value "1" -Force | Out-Null
+
+Copy-Item -Path "$PSScriptRoot\blacklist.txt" -Destination "C:\improsec\blacklist.txt" -Force
+Copy-Item -Path "$PSScriptRoot\$filt.dll" -Destination "C:\Windows\System32\$filt.dll" -Force
+
+if (Test-RegistryValue -Path $path -Key $name) {
+    $values = @((Get-ItemPropertyValue -Path $path -Name $name))
+
+    if (!$values.Contains($filt)) {
+        $values += $filt
+    }
+
+    Set-ItemProperty -Path $path -Name $name -Value $values
+} else {
+    New-ItemProperty -Path $path -Name $name -Value @($filt) -PropertyType MultiString
+}
---
+++ scripts/UninstallFilter.ps1
@@ -0,0 +1,37 @@
+﻿function Test-RegistryValue {
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        $Path,
+        
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        $Key
+    )
+
+    try {
+        Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Key -ErrorAction Stop | Out-Null
+        return $true
+    } catch {
+        return $false
+    }
+}
+
+$path = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
+$name = "Notification Packages"
+$filt = "ipfilter"
+
+Set-Content -Path "C:\improsec\enabled.txt" -Value "0" -Force | Out-Null
+
+if (Test-RegistryValue -Path $path -Key $name) {
+    $oldval = @((Get-ItemPropertyValue -Path $path -Name $name))
+    $newval = @()
+
+    foreach ($v in $oldval) {
+        if ($v -ne $filt) {
+            $newval += $v
+        }
+    }
+
+    Set-ItemProperty -Path $path -Name $name -Value $newval
+}
