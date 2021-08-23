fd0af20bf51f1a57c680f700a7fb0abf18b3a2ff
st4ckh0und <11381503+st4ckh0und@users.noreply.github.com>
2021-08-23 09:47:06 +0200

    Add files via upload

--- ipf_test/main.cpp
+++ ipf_test/main.cpp
@@ -34,62 +34,102 @@ void timed_event(std::string const& description, std::function<void()> functor)
 }
 
 #include <vector>
+#include <array>
 #include <utility>
 
-int main()
+bool nthash(std::wstring const& input, std::array<uint8_t, 16>& digest)
+{
+	HCRYPTPROV hCryptProvider = NULL;
+	HCRYPTHASH hCryptHash = NULL;
+
+	bool result = false;
+
+	if (CryptAcquireContextW(&hCryptProvider, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
+	{
+		if (CryptCreateHash(hCryptProvider, CALG_MD4, 0, 0, &hCryptHash))
+		{
+			if (CryptHashData(hCryptHash, reinterpret_cast<BYTE const*>(input.c_str()), input.size() * 2, 0))
+			{
+				uint8_t md4_hash[16];
+				memset(&md4_hash[0], 0, sizeof(md4_hash));
+
+				uint32_t md4_length = sizeof(md4_hash);
+
+				if (CryptGetHashParam(hCryptHash, HP_HASHVAL, &md4_hash[0], reinterpret_cast<DWORD*>(&md4_length), 0))
+				{
+					memcpy(&digest[0], md4_hash, sizeof(md4_hash));
+					result = true;
+				}
+			}
+
+			CryptDestroyHash(hCryptHash);
+		}
+
+		CryptReleaseContext(hCryptProvider, 0);
+	}
+
+	return result;
+}
+
+int wmain(int argc, wchar_t* argv[])
 {
 	HMODULE hModule = LoadLibrary(L"ipf.dll");
 	
-	if (hModule != NULL)
+	if (hModule == NULL)
+		std::cout << "Cannot load 'filter.dll'" << std::endl;
+	else
 	{
-		timed_event("Initializing filter...", [&]() -> void
-		{
-			InitializeChangeNotify_t initialize = reinterpret_cast<InitializeChangeNotify_t>(GetProcAddress(hModule, "InitializeChangeNotify"));
+		InitializeChangeNotify_t initialize = reinterpret_cast<InitializeChangeNotify_t>(GetProcAddress(hModule, "InitializeChangeNotify"));
 
-			if (initialize == nullptr)
-				std::cerr << "Failed to locate initialization function" << std::endl;
-			else if (!initialize())
-				std::cerr << "Failed to initialize filter" << std::endl;
-		});
+		if (initialize == nullptr)
+			std::cerr << "Failed to locate initialization function" << std::endl;
+		else if (!initialize())
+			std::cerr << "Failed to initialize filter" << std::endl;
 
-		std::vector<std::wstring> v;
+		std::cin.get();
+		
+		timed_event("Scanning for password", [&]() -> void {
+			Passwordfilter_t filter = reinterpret_cast<Passwordfilter_t>(GetProcAddress(hModule, "PasswordFilter"));
 
-		for (std::size_t i = 0; i < 20000; i++)
-		{
-			v.push_back(L"Password123");
-		}
-
-		Sleep(5000);
-
-		for (int i = 0; i < 1; i++)
-		{
-			UNICODE_STRING p;
-
-			timed_event("Testing filter...", [&]() -> void
+			if (filter == nullptr)
+				std::cerr << "Failed to locate filter function" << std::endl;
+			else
 			{
-				Passwordfilter_t filter = reinterpret_cast<Passwordfilter_t>(GetProcAddress(hModule, "Passwordfilter"));
+				wchar_t const* v = L"fakepassword";
 
-				if (filter == nullptr)
-					std::cerr << "Failed to locate filter function" << std::endl;
+				UNICODE_STRING p = { 0 };
+				p.Buffer = const_cast<wchar_t*>(v);
+				p.Length = wcslen(v) * sizeof(WCHAR);
+				p.MaximumLength = p.Length + sizeof(WCHAR);
+
+				if (filter(NULL, NULL, &p, FALSE))
+					std::cout << "ALLOWED" << std::endl;
 				else
-				{
-					for (std::size_t i = 0; i < v.size(); i++)
-					{
-						p.Buffer = const_cast<wchar_t*>(v[i].data());
-						p.Length = static_cast<USHORT>(v[i].size() * sizeof(WCHAR));
-						p.MaximumLength = p.Length + sizeof(WCHAR);
+					std::cout << "FILTERED" << std::endl;
+			}
+		});
+		
+		timed_event("Scanning for password", [&]() -> void {
+			Passwordfilter_t filter = reinterpret_cast<Passwordfilter_t>(GetProcAddress(hModule, "PasswordFilter"));
 
-						if (filter(NULL, NULL, &p, FALSE))
-							std::cout << "ALLOWED" << std::endl;
-						else
-							std::cout << "FILTERED" << std::endl;
-					}
-				}
-			});
-		}
+			if (filter == nullptr)
+				std::cerr << "Failed to locate filter function" << std::endl;
+			else
+			{
+				wchar_t const* v = L"Pa$$w0rd";
+
+				UNICODE_STRING p = { 0 };
+				p.Buffer = const_cast<wchar_t*>(v);
+				p.Length = wcslen(v) * sizeof(WCHAR);
+				p.MaximumLength = p.Length + sizeof(WCHAR);
+
+				if (filter(NULL, NULL, &p, FALSE))
+					std::cout << "ALLOWED" << std::endl;
+				else
+					std::cout << "FILTERED" << std::endl;
+			}
+		});
 	}
-
-	std::cin.ignore();
-	std::cin.get();
+	
 	return 0;
 }
